5b620abf0098c90a4d78fb576107c5caf7f70a22
Adam Chaboryk <adam.chaboryk@gmail.com>
2022-04-11 18:46:09 -0400

    Release of WP plugin!

---
+++ readme.txt
@@ -0,0 +1,56 @@
+=== Sa11y ===
+Contributors: adamchaboryk
+Tags: accessibility, accessibility automated testing, accessibility checker
+Requires at least: 5.6
+Tested up to: 5.9
+Requires PHP: 7.2
+Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Sa11y is an accessibility quality assurance tool that visually highlights common accessibility and usability issues. Geared towards content authors, Sa11y straightforwardly identifies errors or warnings at the source with a simple tooltip on how to fix them.
+
+== Description ==
+
+Sa11y, the accessibility quality assurance assistant. Geared towards content authors, Sa11y straightforwardly identifies errors or warnings at the source with a simple tooltip on how to fix them.
+
+= Features: =
+
+* Over 50 test conditions.
+* Free and open source.
+* Automatic: checks content on page load.
+* Concise tooltips explain issues right at the source.
+* Additional (toggleable) checks: Contrast, form labels, readability, links (Advanced).
+* Dark mode.
+
+View [project website](https://ryersondmp.github.io/sa11y/) for all features.
+
+= Support this project =
+
+* [Translate to your language](https://translate.wordpress.org/projects/wp-plugins/sa11y/).
+* Contribute on [Github](https://github.com/ryersondmp/sa11y/).
+
+== Installation ==
+
+**Through dashboard**
+
+1. Log in to your WordPress admin panel and go to Plugins > Add New
+2. Type **Sa11y** in the search box and click on search button. Find **Sa11y**.
+3. Then click on Install Now.
+4. Activate the plugin.
+5. To customize, go to Settings > Sa11y.
+
+**Installing via FTP**
+
+1. Download the plugin.
+2. Unzip.
+3. Upload the "sa11y" folder into your plugins directory.
+4. Log in to your WordPress admin panel and click the Plugins menu.
+5. Then activate the plugin.
+6. To customize, go to Settings > Sa11y.
+
+== Changelog ==
+
+= 1.0.0 =
+* Initial release of official WordPress plugin.
+
---
+++ sa11y-wp.php
@@ -0,0 +1,96 @@
+<?php
+
+/**
+ * Sa11y, the accessibility quality assurance assistant.
+ * 
+ * Plugin Name:       Sa11y
+ * Plugin URI:        https://ryersondmp.github.io/sa11y
+ * Description:       Sa11y is your accessibility quality assurance assistant. Geared towards content authors, Sa11y straightforwardly identifies accessibility issues at the source.
+ * Version:           1.0.0
+ * Requires at least: 5.6
+ * Requires PHP:      7.2
+ * Author:            Adam Chaboryk, Ryerson University
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       sa11y
+ * Domain Path:       /languages
+ * 
+ * @package         Sa11y
+ * @link            https://ryersondmp.github.io/sa11y
+ * @author          Adam Chaboryk, Ryerson University
+ * @copyright       2022 Ryerson University
+ * @license         GPL v2 or later
+ */
+
+// Exit if accessed directly
+if (!defined('ABSPATH')) exit;
+
+class Sa11y_WP {
+
+    /**
+     * PHP5 constructor method.
+     */
+    public function __construct() {
+
+        // Set the constants needed by the plugin.
+        add_action('plugins_loaded', array(&$this, 'constants'), 1);
+
+        // Internationalize the text strings used.
+        add_action('plugins_loaded', array(&$this, 'i18n'), 2);
+
+        // Load the functions files.
+        add_action('plugins_loaded', array(&$this, 'includes'), 3);
+
+        // Load the admin files.
+        add_action('plugins_loaded', array(&$this, 'admin'), 4);
+    }
+
+    /**
+     * Defines constants used by the plugin.
+     */
+    public function constants() {
+
+        define( 'SA11Y_BASE', plugin_basename( __FILE__ ));
+
+        // Set constant path to the plugin directory.
+        define('SA11Y_DIR', trailingslashit(plugin_dir_path(__FILE__)));
+
+        // Set the constant path to the plugin directory URI.
+        define('SA11Y_URI', trailingslashit(plugin_dir_url(__FILE__)));
+
+        // Set the constant path to the inc directory.
+        define('SA11Y_INCLUDES', SA11Y_DIR . trailingslashit('inc'));
+
+        // Set the constant path to the admin directory.
+        define('SA11Y_ADMIN', SA11Y_DIR . trailingslashit('admin'));
+
+        // Set the constant path to the assets directory.
+        define('SA11Y_ASSETS', SA11Y_URI . trailingslashit('assets'));
+    }
+
+    /**
+     * Loads the translation files.
+     */
+    public function i18n() {
+        load_plugin_textdomain('sa11y-wp', false, dirname(plugin_basename(__FILE__)) . '/languages/');
+    }
+
+    /**
+     * Loads the initial files needed by the plugin.
+     */
+    public function includes() {
+        require_once(SA11Y_INCLUDES . 'functions.php');
+    }
+
+    /**
+     * Loads the admin functions and files.
+     */
+    public function admin() {
+        if (is_admin()) {
+            require_once(SA11Y_ADMIN . 'admin.php');
+        }
+    }
+
+}
+
+new Sa11y_WP;
